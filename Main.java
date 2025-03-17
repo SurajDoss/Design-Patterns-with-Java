97157a633aa16b9b83af009ea00f516e8a782210
Suraj Doss <suraj_sebastien@yahoo.com>
2025-03-18 00:53:08 +0530

    Update Main.java
    Singleton

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ import AbstractFactoryPattern.ToyotaVellFire;
 public class Main {
     public static void main(String[] args) {
         executeAbstractFactoryPattern();
+        executeSingletonPattern();
     }
 
     private static void executeAbstractFactoryPattern(){
@@ -14,5 +15,14 @@ public class Main {
         ICar car = CarFactory.carFactory(CarFactory.Cartype.Tesla);
         CarSuggestor carSuggestor = new CarSuggestor(car);
         carSuggestor.whatCarsIsThis();
+    }
+
+    private static void executeSingletonPattern(){
+        System.out.println("Singleton Pattern");
+
+        ConfigurationManagement configuration = ConfigurationManagement.getInstance();
+        String prop = configuration.getProperty("learned");
+
+        System.out.println("Prop is " + prop);
     }
 }
