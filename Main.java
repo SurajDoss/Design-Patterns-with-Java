8b5473f9488596f01fa2758d4d2e3cdbca8ae26e
Suraj Doss <suraj_sebastien@yahoo.com>
2025-05-21 08:41:52 +0530

    MomentoPattern

--- Main.java
+++ Main.java
@@ -1,12 +1,15 @@
-import AbstractFactoryPattern.CarSuggestor;
-import AbstractFactoryPattern.ICar;
-import AbstractFactoryPattern.TeslaModelS;
-import AbstractFactoryPattern.ToyotaVellFire;
+import AbstractFactoryPattern.*;
+import MomentoPattern.Editor;
+import MomentoPattern.History;
+import SingletonPattern.ConfigurationManagement;
+
+import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) {
-        executeAbstractFactoryPattern();
-        executeSingletonPattern();
+        //executeAbstractFactoryPattern();
+        //executeSingletonPattern();
+        executeMomentoPatter();
     }
 
     private static void executeAbstractFactoryPattern(){
@@ -24,5 +27,24 @@ public class Main {
         String prop = configuration.getProperty("learned");
 
         System.out.println("Prop is " + prop);
+    }
+
+    private static void executeMomentoPatter(){
+        System.out.println("Momento Pattern");
+
+        Editor editor = new Editor();
+        History history =  new History();
+
+        editor.setContent("a");
+        history.push(editor.createState());
+
+        editor.setContent("b");
+        history.push(editor.createState());
+
+        editor.setContent("c");
+        editor.restore(history.pop());
+        editor.restore(history.pop());
+
+        System.out.println(editor.getContent());
     }
 }
